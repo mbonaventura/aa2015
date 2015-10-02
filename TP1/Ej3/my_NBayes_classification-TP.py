e2a3062a23d6e946751ddfb8ebcac27bf6c55f9a
GT <tolosoft@gmail.com>
2015-10-02 14:41:54 -0300

    Ejer 3 casi listo

--- TP1/Ej3/my_NBayes_classification-TP.py
+++ TP1/Ej3/my_NBayes_classification-TP.py
@@ -3,9 +3,7 @@ import re, sys
 import codecs
 import itertools
 
-import random
 import numpy as np
-
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -14,15 +12,15 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import *
 from sklearn.cross_validation import train_test_split
 
+def setPositiveClass(topics, positive_class):
+    num_class = []
+    for top in topics:
+        if (top == positive_class):
+            num_class.append(1)
+        else:
+            num_class.append(0)
+    return num_class 
 
-def getDocuments(doc_iter, size, pos_class):
-    data = [(u'{title}\n\n{body}'.format(**doc), pos_class in doc['topics'])
-            for doc in itertools.islice(doc_iter, size)
-            if doc['topics']]
-    if not len(data):
-        return np.asarray([], dtype=int), np.asarray([], dtype=int)
-    X_text, y = zip(*data)
-    return X_text, np.asarray(y, dtype=int)
 
 #------------------------------------------------------------------------------------------------
 # Main
@@ -34,32 +32,33 @@ hfile = codecs.open(infile, encoding='utf-8')
 
 vec = sys.argv[2]
 #
+X_data = []
+Y_data = []
 
-#
-data = []
-line =""
 for line in hfile.readlines():
     line_array = line.strip().split(';')
-    #
-    line_dict = {}  
-    line_dict['body'] = line_array[3]
-    line_dict['topics'] = [line_array[2].strip()]
-    line_dict['title'] = line_array[4]
-    data.append(line_dict)
+    X_data.append(line_array[3] + ' ' + line_array[4])
+    
+    this_class = line_array[2].strip()
+    
+    if (this_class in ('politica', 'internacionales', 'seguridad', 'economia')):
+        this_class = 'resto'
+    if (this_class in ('espectaculos', 'tecnologia', 'sociedad', 'cultura')):
+        this_class = 'resto'
+
+    Y_data.append(this_class)
+
+
+
+
 #
-
-#random.shuffle(data)
-
-
-data_stream = data
-total_documents = len(data)
 vectorizer = ''
 # Create the vectorizer and limit the number of features to a reasonable maximum
 # El valor original de n_features es 2^18. stop_words: string {english}, list, or None (default)
 #
 if vec == 'hash':
     print 'Inicializando hashing vectorizer...'
-    vectorizer = HashingVectorizer(decode_error='ignore', n_features=2 ** 20, non_negative=True, binary=False)
+    vectorizer = HashingVectorizer(decode_error='ignore', n_features=2 ** 14, non_negative=True, binary=False)
 #
 if vec == 'tfidf':
     print 'Inicializando tfidf vectorizer...'
@@ -71,10 +70,7 @@ if vec == 'count':
 #
 #
 class_names = ['cultura', 'deportes', 'economia', 'espectaculos', 'internacionales', 'politica', 'seguridad', 'sociedad','tecnologia']
-#class_names = ['deportes']
-#class_names = ['sociedad','deportes', 'espectaculos', 'internacionales']
-#
-
+class_names = ['deportes', 'resto']
 
 sum_precision = 0
 sum_recall = 0
@@ -86,26 +82,28 @@ print '-------------------------------------------------------------------------
 #
 for positive_class in class_names:
     
-    X_all_text, y_all = getDocuments(data_stream, total_documents, positive_class)    
-    X_train_text, X_test_text, y_train, y_test = train_test_split(X_all_text, y_all, test_size=.3, random_state=0)
+    X_train_text, X_test_text, y_train, y_test = train_test_split(X_data, Y_data, test_size=.3, random_state=0)
 
     #
     X_train = []
     X_test = []
 
     if vec == 'hash':
-        X_train = vectorizer.transform(X_train_text)
-        X_test  = vectorizer.transform(X_test_text)
+        X_train = vectorizer.transform(X_train_text)        
     #
     if vec == 'tfidf':
         X_train = vectorizer.fit_transform(X_train_text)
-        X_test  = vectorizer.transform(X_test_text)
     #
     if vec == 'count':
         X_train = vectorizer.fit_transform(X_train_text)
-        X_test  = vectorizer.transform(X_test_text)
     #
+    X_test  = vectorizer.transform(X_test_text)
     #
+
+    y_train = setPositiveClass(y_train, positive_class) 
+    y_test  = setPositiveClass(y_test, positive_class)
+    #
+
     #cls = MultinomialNB(alpha=0.01) # alpha: Additive (Laplace/Lidstone) smoothing parameter (0 for no smoothing).
     cls = MultinomialNB()
     y_pred = cls.fit(X_train, y_train).predict(X_test)
@@ -121,14 +119,6 @@ for positive_class in class_names:
     print '%15s\t%4d/%d\t%4d/%d\t%0.4f\t%0.4f\t%0.4f\t%0.4f' % (positive_class, sum(y_train), len(y_train), sum(y_test), len(y_test), this_accuracy, this_precision, this_recall, this_f1),
     print '\t',cm[0], cm[1]
 
-    #print("Clase (%s) - Train %d/%d items - Test %d/%d items" % (positive_class, sum(y_train), len(y_train), sum(y_test), len(y_test))), '\t', 
-    #print 'Accuracy:  %0.4f' % accuracy_score(y_test, y_pred), '\t',
-    #print 'Precision: %0.4f' % this_precision, '\t',
-    #print 'Recall: ', this_recall, '\t',
-    #print 'F1: ', this_f1, ''
-    #
-    #print 'Matriz de confusion'
-    
     #
     if tcm==[]:
         tcm=cm
