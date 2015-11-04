166a385558a89858b0d82fda0e95bd78d8ca1a35
matiasb <matiasb@gmail.com>
2015-11-04 13:37:21 -0300

    added TP2

---
+++ TP2/cats-and-dogs.py
@@ -0,0 +1,90 @@
+import numpy as np
+import mahotas as mh
+from mahotas.features import surf
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import *
+from sklearn.cluster import MiniBatchKMeans
+import glob
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+
+all_instance_filenames = []
+all_instance_targets = []
+for f in glob.glob('./img2/*.jpg'):
+	target = 1 if 'cat' in f else 0
+	all_instance_filenames.append(f)
+	all_instance_targets.append(target)
+	#print f, target
+
+
+#sys.exit
+
+surf_features = []
+counter = 0
+for f in all_instance_filenames:
+	counter+=1
+	#print 'Reading image: ', counter, f
+	print counter 
+	image = mh.imread(f, as_grey=True)
+	surf_features.append(surf.surf(image)[:, 5:])
+	#print surf.surf(image)
+	#print len(surf_features[0])
+#
+
+a = np.asarray(surf_features)
+a.tofile('surf_features.csv', sep=',', format='%10.5f')
+
+#
+
+train_len = int(len(all_instance_filenames) * .70)
+X_train_surf_features = np.concatenate(surf_features[:train_len])
+X_test_surf_feautres = np.concatenate(surf_features[train_len:])
+y_train = all_instance_targets[:train_len]
+y_test = all_instance_targets[train_len:]
+
+#
+n_clusters = 300
+print 'Clustering', len(X_train_surf_features), 'features'
+estimator = MiniBatchKMeans(n_clusters=n_clusters)
+estimator.fit_transform(X_train_surf_features)
+
+#
+X_train = []
+for instance in surf_features[:train_len]:
+	clusters = estimator.predict(instance)
+	features = np.bincount(clusters)
+	if len(features) < n_clusters:
+		features = np.append(features, np.zeros((1, n_clusters-len(features))))
+
+	X_train.append(features)
+
+X_test = []
+for instance in surf_features[train_len:]:
+	clusters = estimator.predict(instance)
+	features = np.bincount(clusters)
+	if len(features) < n_clusters:
+		features = np.append(features, np.zeros((1, n_clusters-len(features))))
+	X_test.append(features)	
+#
+#
+#
+clf = LogisticRegression(C=0.001, penalty='l2')
+clf.fit_transform(X_train, y_train)
+
+#clf = tree.DecisionTreeClassifier()
+#clf = RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1)
+#clf = RandomForestClassifier()												
+#clf = AdaBoostClassifier()
+#clf = SVC(kernel="linear", C=0.025)
+#clf = SVC(gamma=2, C=1)
+#clf = GaussianNB()
+
+clf.fit(X_train, y_train)
+
+predictions = clf.predict(X_test)
+print classification_report(y_test, predictions)
+print 'Precision: ', precision_score(y_test, predictions)
+print 'Recall: ', recall_score(y_test, predictions)
+print 'Accuracy: ', accuracy_score(y_test, predictions)
---
+++ TP2/clasi-surf.py
@@ -0,0 +1,62 @@
+import sys
+import numpy as np
+from sklearn import cross_validation
+
+
+import mahotas as mh
+from mahotas.features import surf
+
+
+#import mahotas as mh
+#from mahotas.features import surf
+#from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import *
+#from sklearn.cluster import MiniBatchKMeans
+import glob
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+#from sklearn.svm import SVC
+#from sklearn.naive_bayes import GaussianNB
+import cv2
+
+
+
+#-----------------------------------------------------------------------------
+all_instance_filenames = []
+targets_data = []
+for f in glob.glob('./img2/*.jpg'):
+	target = 1 if 'cat' in f else 0
+	all_instance_filenames.append(f)
+	targets_data.append(target)
+	#print f, target
+
+x_data = []
+counter = 0
+for f in all_instance_filenames:
+	counter+=1
+	#print 'Reading image: ', counter, f
+	image = mh.imread(f, as_grey=True)
+	surf_features  = surf.surf(image)[:, 5:]
+	#
+	#print surf.surf(image).shape
+	#print surf_features.shape
+	x_data.append(surf_features)
+#
+
+print x_data[0]
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(x_data, targets_data, test_size=0.3, random_state=0)
+
+#print X_train[0]
+
+clf = RandomForestClassifier()
+
+clf.fit(X_train, y_train)
+
+predictions = clf.predict(X_test)
+
+print classification_report(y_test, predictions)
+print 'Precision: ', precision_score(y_test, predictions)
+print 'Recall: ', recall_score(y_test, predictions)
+print 'Accuracy: ', accuracy_score(y_test, predictions)
+	
