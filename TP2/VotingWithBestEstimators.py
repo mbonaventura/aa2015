5ce2e465cd45da9d1b501cf575fcb263d13d5f88
matiasb <matiasb@gmail.com>
2015-11-25 01:42:51 -0300

    Terminado el Cap3

--- TP2/VotingWithBestEstimators.py
+++ TP2/VotingWithBestEstimators.py
@@ -10,9 +10,10 @@ from sklearn.svm import SVC
 from classifier_search import *
 from attribute_extraction import *
 
-featureSet = 'surf-c300-norm' # 'bp-r5' 'hara-img200'
+
 
 # Get attributes and class for the images
+featureSet = 'surf-c300-norm' # 'bp-r5' 'hara-img200'
 x_data, targets_data = getAttributes(featureSet)
 
 # Separate in train & test
@@ -20,11 +21,17 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(x_data, tar
 
 # create the estimators with corresponding parameters
 estimators = []
+scores = []
 estimators.append(('LogisticRegression', LogisticRegression(penalty='l2', C=0.005))) # expected 0.774
+scores.append(0.774)
 estimators.append(('SVC', SVC(kernel='rbf', C=100, gamma=0.001))) # expected 0.758
+scores.append(0.758)
 estimators.append(('GradientBoostingClassifier', GradientBoostingClassifier(n_estimators=150, loss='deviance', learning_rate=0.1, max_depth=3))) # expected 0.728
+scores.append(0.728)
 estimators.append(('BaggingClassifier', BaggingClassifier(max_features=0.5, max_samples=0.5, base_estimator=KNeighborsClassifier()))) # expected 0.718
+scores.append(0.718)
 estimators.append(('RandomForestClassifier', RandomForestClassifier(max_features=100, n_estimators=100, max_depth=100))) # expected 0.709
+scores.append(0.709)
 
 # Voting
 voting = VotingClassifier(estimators=estimators, voting='hard');
@@ -41,7 +48,6 @@ print 'Recall: ', recall_score(y_test, voting_prediction)
 print 'Accuracy: ', accuracy_score(y_test, voting_prediction)
 saveEstimator(voting, precision_score(y_test, voting_prediction), './estimators/%s_votingHard' % featureSet) 
 
-scores = [score for (estimator, score) in bestEstimators]
 voting = VotingClassifier(estimators=estimators, voting='soft', weights=scores);
 voting.fit(X_train, y_train)
 voting_prediction = voting.predict(X_test)
@@ -54,8 +60,3 @@ print 'Precision: ', precision_score(y_test, voting_prediction)
 print 'Recall: ', recall_score(y_test, voting_prediction)
 print 'Accuracy: ', accuracy_score(y_test, voting_prediction)
 saveEstimator(voting, precision_score(y_test, voting_prediction), './estimators/%s_votingSoft' % featureSet) 
-
-print("========================================================================================")
-print("========================================================================================")
-print("The very best best estimator is (or de Votings?): (Precision %f )" % bestbest_score)
-print(bestbest_estimator)
